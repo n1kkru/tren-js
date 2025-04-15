b4eb268bbf28fd771f0cc122cc66240503795131
nikita.k.dterra <klukoyt@dterra.ru>
2025-04-15 20:06:59 +0300

    upd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
+import path, { resolve } from 'node:path'
+import url from 'node:url'
+
+const timestamp = Date.now()
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,7 +23,27 @@ export default defineConfig({
 
   vite: {
     build: {
-      cssCodeSplit: false
+      cssCodeSplit: false,
+      rollupOptions: {
+        output: {
+          manualChunks: () => 'main',
+          entryFileNames: `scripts/main.js`,
+          chunkFileNames: `scripts/[name].js`,
+          assetFileNames: assetInfo => {
+            let extType = assetInfo.name.split('.')[1]
+
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+              extType = 'images'
+            } else if (extType === 'css') {
+              extType = 'styles'
+            } else if (extType === 'js') {
+              extType = 'scripts'
+            }
+
+            return `${extType}/[name][extname]`
+          }
+        }
+      }
     },
     css: {
       preprocessorOptions: {
