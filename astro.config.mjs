a91f4e76f4809098cf4967eba4b6e01d9109736a
nikita.k.dterra <klukoyt@dterra.ru>
2025-04-15 21:32:17 +0300

    upd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,46 @@
 // @ts-check
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
-import path, { resolve } from 'node:path'
-import url from 'node:url'
+import fs from 'fs/promises'
+import path from 'path'
 
 const timestamp = Date.now()
+
+const timestampIntegration = () => ({
+  name: 'timestamp-integration',
+  hooks: {
+    'astro:build:done': async ({ dir, pages }) => {
+      try {
+        for (const page of pages) {
+          const filePath = path
+            .join(dir.pathname, page.pathname, 'index.html')
+            .replace(/([^:])\/\//g, '$1/')
+            .slice(1)
+
+          try {
+            await fs.access(filePath)
+          } catch {
+            console.log(`Файл не найден: ${filePath}`)
+            continue
+          }
+
+          let content = await fs.readFile(filePath, 'utf-8')
+          const updatedContent = content.replace(
+            /(<script\b[^>]*\bsrc=")([^"]*\.js)("[^>]*>)/g,
+            `$1$2?${timestamp}$3`
+          )
+
+          if (updatedContent !== content) {
+            await fs.writeFile(filePath, updatedContent)
+            console.log(`Обновлен: ${path.join(page.pathname, 'index.html')}`)
+          }
+        }
+      } catch (error) {
+        console.error('Ошибка:', error)
+      }
+    }
+  }
+})
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,7 +55,7 @@ export default defineConfig({
 
   compressHTML: false,
 
-  integrations: [tailwind()],
+  integrations: [tailwind(), timestampIntegration()],
 
   vite: {
     build: {
